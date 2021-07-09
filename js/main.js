1d9b204211c3fc25be9051dde32c0479b6ed8107
SardorTuraev <sardortura2255@gmail.com>
2021-07-09 09:27:41 +0500

    finished

--- js/main.js
+++ js/main.js
@@ -1,7 +1,35 @@
-var elToggleButton = document.querySelector(".site-header__toggle");
-var elMenu = document.querySelector(".site-header");
+
+var elSiteHeaderSection = document.querySelector(".site-header");
+var elSiteHeaderToggler = elSiteHeaderSection.querySelector(".site-header__toggle");
+
+if (elSiteHeaderToggler) {
+  elSiteHeaderToggler.addEventListener("click", function () {
+    elSiteHeaderSection.classList.toggle("site-header--active");
+  });
+}
 
 
-elToggleButton.addEventListener("click", function () {
-  elMenu.classList.toggle("site-header--active");
-});
+
+var elsOrderSection = document.querySelectorAll(".order-section");
+var elsOrderButtonToggler = document.querySelectorAll('.order-section-toggler');
+
+elsOrderButtonToggler.forEach( function (button) {
+  button.addEventListener('click', function () {
+    button.closest('.order-section').classList.toggle('order-section--active');
+
+  })
+})
+
+
+var elBtnCreatePlan = document.querySelector(".btn-create");
+var elOrderModal = document.querySelector('.order-modal');
+elBtnCreatePlan.addEventListener("click", function () {
+  elOrderModal.classList.add('order-modal--active');
+})
+
+var elOrderModalActiveSection = document.querySelector(".order-modal--active");
+
+
+elOrderModal.addEventListener("click", function () {
+    elOrderModal.classList.remove("order-modal--active");
+})
